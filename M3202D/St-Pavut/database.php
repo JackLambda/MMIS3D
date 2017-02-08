10c97c1134b9de9b7602b408fcb74ef6109d180a
Paomine <coralieprudhommeaux@gmail.com>
2017-02-08 15:14:49 +0100

    Create database.php

---
+++ M3202D/St-Pavut/database.php
@@ -0,0 +1,23 @@
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+        'dsn'   => '',
+        'hostname' => 'localhost',
+        'username' => 'mmid116bXX',
+        'password' => '',
+        'database' => 'database_name',
+        'dbdriver' => 'mysqli',
+        'dbprefix' => '',
+        'pconnect' => TRUE,
+        'db_debug' => TRUE,
+        'cache_on' => FALSE,
+        'cachedir' => '',
+        'char_set' => 'utf8',
+        'dbcollat' => 'utf8_general_ci',
+        'swap_pre' => '',
+        'encrypt' => FALSE,
+        'compress' => FALSE,
+        'stricton' => FALSE,
+        'failover' => array()
+);
